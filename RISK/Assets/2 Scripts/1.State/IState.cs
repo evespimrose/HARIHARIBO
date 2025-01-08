b6457d5b339e01a88af6ecdf5cfac5f0d15ffb14
RJH7387 <abj2563@naver.com>
2025-01-08 11:38:06 +0900

    [추가] State

--- RISK/Assets/2 Scripts/1.State/IState.cs
+++ RISK/Assets/2 Scripts/1.State/IState.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public interface IState<T> where T : BaseCharacter
+public interface IState<T> where T : class
 {
     void Enter(T entity);
     void Update(T entity);
