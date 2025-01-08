9b80b0866652da238b84b5191a87d59aa83104cc
evespimrose <evespimrose@users.noreply.github.com>
2025-01-08 17:46:10 +0900

    [변경] TitleScene - UI
    UI Rect 타입 변경. 추후 해상도 변경 대응 위함.

--- RISK/Assets/2 Scripts/UI/Title/CreateCharacterPanel.cs
+++ RISK/Assets/2 Scripts/UI/Title/CreateCharacterPanel.cs
@@ -1,18 +1,78 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CreateCharacterPanel : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private List<ClassNameToCharacterData> classDataList;
+
+    private Dictionary<ClassName, CharacterData> diccd;
+
+    public Image characterModelImage;
+    public Image characterImage;
+    public TMP_InputField nickNameInputfield;
+    public Button createButton;
+    public Button closeButton;
+
+    private void Awake()
     {
-        
+        // List를 Dictionary로 변환
+        diccd = new Dictionary<ClassName, CharacterData>();
+        foreach (var entry in classDataList)
+        {
+            if (!diccd.ContainsKey(entry.className))
+            {
+                diccd.Add(entry.className, entry.characterData);
+            }
+        }
+
+        createButton.onClick.AddListener(OnCreateButtonClick);
+        closeButton.onClick.AddListener(OnCloseButtonClick);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnCloseButtonClick()
     {
-        
+        PanelManager.Instance.PanelOpen("SelectCharacter");
+    }
+
+    private void OnCreateButtonClick()
+    {
+        // 캐릭터 생성 로직
     }
 }
+
+
+public enum ClassName
+{
+    None,
+    Warrior,
+    SpearMan,
+    Archer,
+    Mage,
+}
+[Serializable]
+public class CharacterData
+{
+    public string description;
+    public string characteristic;
+}
+public class FireBaseCharacterData
+{
+    public string nickName = null;
+    public int className;
+    public string maxHp;
+    public string moveSpeed;
+
+}
+
+[Serializable]
+public class ClassNameToCharacterData
+{
+    public ClassName className;
+    public CharacterData characterData;
+}
+
--- RISK/Assets/2 Scripts/UI/Title/SelectCharacterPanel.cs
+++ RISK/Assets/2 Scripts/UI/Title/SelectCharacterPanel.cs
@@ -1,18 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SelectCharacterPanel : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Button deleteButton;
+    public Button closeButton;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public Button selectButton;
+    public Image characterModelImage;
+
+    public Transform characterListTransform;
+
+    public GameObject characterDataPrefab;
+    public Button characterCreatePrefab;
 }
